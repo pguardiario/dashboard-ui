8632301589617b24bafdaa017d678e376312b1dc
P Guardiario <p.guardiario@gmail.com>
2024-10-01 09:11:48 +0800

    lineitems

--- sync.js
+++ sync.js
@@ -70,8 +70,8 @@ async function updateLineItem(lineItem, invoiceID, date){
     return
   }
 
-  if(!accountCode){
-    // debugger
+  if(!unitAmount){
+    return
   }
 
   record = await prisma.lineItems.create({data: {
@@ -247,7 +247,7 @@ async function syncInvoice(invoice) {
 
 async function run() {
   const { access_token, expires_at } = await xero.getClientCredentialsToken()
-  for (let i = 1; i < 9999; i++) {
+  for (let i = 14; i < 9999; i++) {
     console.log(i)
     let data = await fetch(`https://api.xero.com/api.xro/2.0/Invoices?page=${i}&pageSize=100`, {
       headers: {
